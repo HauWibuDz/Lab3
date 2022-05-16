217831842f4c52d44850c9a192784f934d5d9229
Hautrung1608 <105596721+HauWibuDz@users.noreply.github.com>
2022-05-16 22:05:40 +0700

    Add files via upload

---
+++ Bai1-lap3/src/add.ts
@@ -0,0 +1,114 @@
+var myData: Array<any> = [
+    { code:'B1234',lop:'C2110H1',name:"Hoàng Bá Hào",email:'hao@gmail.com',password:'123456', toan: 8, ly: 10, hoa:7},
+    { code:'B1235',lop:'C2110H2',name:"Trần Ngọc Đào",email:'daongoc@gmail.com',password:'123456',toan: 5, ly: 7,hoa: 7},
+    { code:'B1236',lop:'C2110i1',name: "Vũ Văn Kiên",email:'kien@gmail.com',password:'123456', toan: 4,ly: 6,hoa:7},
+    { code:'B1237',lop:'C2110H1',name:"Lê Trung Tín",email: 'tintrung@gmail.com',password:'123456',toan: 6,ly: 5,hoa: 4 },
+    { code:'B1238',lop:'C2110i1',name: "Đào Ngọc Hòa",email: 'hoadn@gmail.com', password: '123456',toan: 8, ly: 10, hoa:5},
+    { code:'B1239',lop:'C2110H2',name:"Lê Văn Vinh",email: 'vinlv@gmail.com',password:'123456',toan: 5,ly: 7,hoa: 3 },
+    { code:'B1240',lop:'C2110H1',name:"Đỗ Đức Hiếu",email: 'hieudd@gmail.com',password:'123456',toan: 8,ly: 10,hoa: 9 },
+    { code:'B1241',lop:'C2110i2',name:"Tào Công Long",email: 'longtao@gmail.com',password:'123456',toan: 8,ly: 10,hoa:7},
+    { code:'B1242',lop:'C2110G1',name:"Vũ Chí Bảo",email: 'baovc@gmail.com',password: '123456',toan: 8,ly: 10,hoa: 7 }
+];
+function myDatas(): Array<any> {
+    return[
+        { code:'B1234',lop:'C2110H1',name:"Hoàng Bá Hào",email:'hao@gmail.com',password:'123456', toan: 8, ly: 10, hoa:7},
+    { code:'B1235',lop:'C2110H2',name:"Trần Ngọc Đào",email:'daongoc@gmail.com',password:'123456',toan: 5, ly: 7,hoa: 7},
+    { code:'B1236',lop:'C2110i1',name: "Vũ Văn Kiên",email:'kien@gmail.com',password:'123456', toan: 4,ly: 6,hoa:7},
+    { code:'B1237',lop:'C2110H1',name:"Lê Trung Tín",email: 'tintrung@gmail.com',password:'123456',toan: 6,ly: 5,hoa: 4 },
+    { code:'B1238',lop:'C2110i1',name: "Đào Ngọc Hòa",email: 'hoadn@gmail.com', password: '123456',toan: 8, ly: 10, hoa:5},
+    { code:'B1239',lop:'C2110H2',name:"Lê Văn Vinh",email: 'vinlv@gmail.com',password:'123456',toan: 5,ly: 7,hoa: 3 },
+    { code:'B1240',lop:'C2110H1',name:"Đỗ Đức Hiếu",email: 'hieudd@gmail.com',password:'123456',toan: 8,ly: 10,hoa: 9 },
+    { code:'B1241',lop:'C2110i2',name:"Tào Công Long",email: 'longtao@gmail.com',password:'123456',toan: 8,ly: 10,hoa:7},
+    { code:'B1242',lop:'C2110G1',name:"Vũ Chí Bảo",email: 'baovc@gmail.com',password: '123456',toan: 8,ly: 10,hoa: 7 }
+    ]
+}
+// Tạo hàm in danh sách mảng dữ liệu sinh viên lên bảng html để tái sử dụng như sau
+var showStudentList = function (data: any) {
+    var _html = '';
+    // duyệt mảng
+    for (let std of data) {
+        var tongDiem = std.toan + std.ly + std.hoa;
+        let diemTB = tongDiem / 3; //làm tròn kết quả lên 1 dấu
+        let xepLoai = '';
+        if (diemTB > 5 && diemTB < 6.5) {
+        xepLoai = 'Trung Bình';
+        } else if (diemTB >= 6.5 && diemTB < 8) {
+        xepLoai = 'Khá';
+        } else if (diemTB >= 8 && diemTB < 9) {
+        xepLoai = 'Giỏi';
+        } else if (diemTB >= 9) {
+        xepLoai = 'Xuất sắc';
+        } else {
+        xepLoai = 'Yếu kém';
+        }
+        _html += `<tr>
+        <td>${std.code}</td>
+        <td>${std.lop}</td>
+        <td>${std.name}</td>
+        <td>${std.email}</td>
+        <td>${std.toan}</td>
+        <td>${std.ly}</td>
+        <td>${std.hoa}</td>
+        <td>${diemTB.toFixed(1)}</td>
+        <td>${xepLoai}</td>
+        </dtr> `;
+        }
+    var _bodyList: any = document.getElementById('tbody-list');
+    _bodyList.innerHTML = _html;
+}
+    // Gọi lại hàm
+showStudentList(myData);
+    // truy cập các điều khiển tren form
+var input_marks: any = document.getElementById('input_marks');
+var btn_filter: any = document.getElementById('btn_filter');
+
+btn_filter.onclick = function () {
+    let marks = input_marks.value;
+    // kiemr tra xem đã nhập giá trị chưa
+    if (marks == '') {
+        alert('VUi lòng nhập ddiemr cần tìm');
+        return;
+    }
+    // sử dụng hàm filter để lọc theo điều kiện
+    let studentFIlters = myData.filter(function (obj) {
+        var totalMarks = obj.toan + obj.ly + obj.hoa;
+        let markAvg = totalMarks / 3; //làm tròn kết quả lên 1 dấu
+        return markAvg.toFixed(1) >= marks;
+    });
+    showStudentList(studentFIlters); // gọi lại hàm in dữ liệu sau khi lọc kết qu
+}
+// khai báo hàm sort_markAvg kiể arrow function
+var sort_markAvg = (sort_type: string) => {
+    let _myData = myDatas(); // load lại dữ liệu mỗi làn click
+    if (sort_type == 'ASC') {
+    let studenSort = myData.sort(function (a: any, b: any) {
+    return a.toan < b.toan ? -1 : 0;
+    });
+    showStudentList(studenSort);
+    } else if (sort_type == 'DESC') {
+    let studenSort = myData.sort(function (a: any, b: any) {
+    return a.toan > b.toan ? -1 : 0;
+    });
+    showStudentList(studenSort);
+    } else {
+    // Gọi lại hàm
+    showStudentList(myData);
+    }
+    }
+
+var input_class: any = document.getElementById('input_class');
+var btn_filter_class: any = document.getElementById('btn_filter_class');
+
+btn_filter_class.onclick = function () {
+    let classes = input_class.value;
+    // kiemr tra xem đã nhập giá trị chưa
+    if (classes == '') {
+        alert('VUi lòng nhập Class cần tìm');
+        return;
+    }
+    // sử dụng hàm filter để lọc theo điều kiện
+    let studentFIlters = myData.filter(function (obj) {
+        return classes == obj.lop;
+    });
+    showStudentList(studentFIlters); // gọi lại hàm in dữ liệu sau khi lọc kết qu
+}
---
+++ Bai2-lap3/src/app.ts
@@ -0,0 +1,164 @@
+var data: Array<any> = [
+    {
+        name: "Conan",
+        category_name: "Manga",
+        price: 30000,
+        sale_price: 21000,
+        image: "../imgs/conan.jpg",
+    },
+    {
+        name: "Doraemon",
+        category_name: "Manga",
+        price: 30000,
+        sale_price: 22000,
+        image: "../imgs/doraemon.jpg",
+    },
+    {
+        name: "Khu Vườn ngôn từ",
+        category_name: "LightNovel",
+        price: 150000,
+        sale_price: 95000,
+        image: "../imgs/Khu_vuon_ngon_tu.jpg",
+    },
+    {
+        name: "Your Name",
+        category_name: "LightNovel",
+        price: 150000,
+        sale_price: 117000,
+        image: "../imgs/your_name.jpg",
+    },
+    {
+        name: "Kimetsu No Yaiba",
+        category_name: "Manga",
+        price: 200000  ,
+        sale_price: 169000,
+        image: "../imgs/Kimetsu_no_yaiba.jpg",
+    },
+    {
+        name: "Nanatsu No Yaiba",
+        category_name: "Manga",
+        price: 40000,
+        sale_price: 25000,
+        image: "../imgs/Nanatsu_no_Taizai.jpg",
+    },
+];
+
+function myDatas():Array<any> {
+    return [
+        {
+            name: "Conan",
+            category_name: "Manga",
+            price: 30000,
+            sale_price: 21000,
+            image: "../imgs/conan.jpg",
+        },
+        {
+            name: "Doraemon",
+            category_name: "Manga",
+            price: 30000,
+            sale_price: 22000,
+            image: "../imgs/doraemon.jpg",
+        },
+        {
+            name: "Khu Vườn ngôn từ",
+            category_name: "LightNovel",
+            price: 150000,
+            sale_price: 95000,
+            image: "../imgs/Khu_vuon_ngon_tu.jpg",
+        },
+        {
+            name: "Your Name",
+            category_name: "LightNovel",
+            price: 150000,
+            sale_price: 117000,
+            image: "../imgs/your_name.jpg",
+        },
+        {
+            name: "Kimetsu No Yaiba",
+            category_name: "Manga",
+            price: 200000,
+            sale_price: 169000,
+            image: "../imgs/Kimetsu_no_yaiba.jpg",
+        },
+        {
+            name: "Nanatsu No Yaiba",
+            category_name: "Manga",
+            price: 40000,
+            sale_price: 25000,
+            image: "../imgs/Nanatsu_no_Taizai.jpg",
+        },
+    ];
+};
+
+var showProductList = function (data: any) {
+    var _html = '';
+    for (let key of data) {
+        _html += `
+            <tr>
+                <th scope="row">${key.name}</th>
+                <td>${key.category_name}</td>
+                <td><del>${key.price}</del></td>
+                <td>${key.sale_price}</td>
+                <td><img src="${key.image}"/></td>
+            </tr>
+        `;
+    }
+    var _bodyList:any = document.getElementById("tbody-list");
+    _bodyList.innerHTML = _html;
+}
+showProductList(data);
+
+var input_cate: any = document.getElementById('input_cate');
+var btn_filter_cate:any = document.getElementById('btn');
+
+btn_filter_cate.onclick = function () {
+    let _myData = myDatas();
+    let cates = input_cate.value;
+
+    if (cates == '') {
+        alert('Vui Long Nhap Category Name');
+        return;
+    }
+
+    let ProductFilter = data.filter(function (obj) {
+        return cates == obj.category_name;
+    });
+    showProductList(ProductFilter);
+};
+
+var sortPrices = (sort_type: string) => {
+    let _myData = myDatas();
+    if (sort_type == 'ascending') {
+        let priceSort = data.sort(function (a: any, b: any) {
+            return a.price < b.price ? -1 : 0;
+        });
+        showProductList(priceSort);
+    } else if (sort_type == 'descending') {
+        let priceSort = data.sort(function (a: any, b: any) {
+            return a.price > b.price ? -1 : 0;
+        });
+        showProductList(priceSort);
+    } else {
+        showProductList(data);
+        return;
+    }
+};
+
+var input_f:any = document.getElementById("input_first");
+var input_s:any = document.getElementById("input_second");
+var btn_sort:any = document.getElementById('btn_sort');
+
+btn_sort.onclick = function () {
+    let first = input_f.value;
+    let second = input_s.value;
+
+    if (first == '' || second == '') {
+        alert('Vui Long Nhap Gia Tri');
+        return;
+    }
+
+    let Filter = data.filter(function (obj) {
+        return first <= obj.sale_price && obj.sale_price <= second; 
+    });
+    showProductList(Filter);
+}
